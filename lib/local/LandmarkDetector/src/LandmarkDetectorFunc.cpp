56dac1d3a662fa7125d6ee7e08a127b60bda6d18
TadasBaltrusaitis <tadyla@gmail.com>
2017-02-08 11:34:23 -0500

    Another DCLM speedup.

--- lib/local/LandmarkDetector/src/LandmarkDetectorFunc.cpp
+++ lib/local/LandmarkDetector/src/LandmarkDetectorFunc.cpp
@@ -469,8 +469,13 @@ bool LandmarkDetector::DetectLandmarksInImage(const cv::Mat_<uchar> &grayscale_i
 		rotation_hypotheses.push_back(cv::Vec3d(0,0,0));
 		rotation_hypotheses.push_back(cv::Vec3d(0,0.5236,0));
 		rotation_hypotheses.push_back(cv::Vec3d(0,-0.5236,0));
-		rotation_hypotheses.push_back(cv::Vec3d(0.5236,0,0));
-		rotation_hypotheses.push_back(cv::Vec3d(-0.5236,0,0));
+
+		// If not using DPN patch experts add additional hypotheses
+		if(clnf_model.patch_experts.dpn_expert_intensity.size() == 0)
+		{
+			rotation_hypotheses.push_back(cv::Vec3d(0.5236,0,0));
+			rotation_hypotheses.push_back(cv::Vec3d(-0.5236,0,0));
+		}
 	}
 	else
 	{
