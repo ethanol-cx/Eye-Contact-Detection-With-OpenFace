c7e13bce9e92e5b5a07686b2e41fc26da4fc28df
Tadas Baltrusaitis <tadyla@gmail.com>
2017-11-14 07:46:01 +0000

    Bringing the ICT-3DHP script up to date with the new interface

--- matlab_runners/Head Pose Experiments/run_ict_experiment.m
+++ matlab_runners/Head Pose Experiments/run_ict_experiment.m
@@ -11,43 +11,29 @@ end
 output_dir = 'experiments/ict_out';    
 
 dbSeqDir = dir([rootDir ictDir]);
-   
+dbSeqDir = dbSeqDir(3:end);
+
 output_dir = cat(2, output_dir, '/');
 
-numTogether = 10;
+command = sprintf('%s -inroot "%s" -outroot "%s" -fx 535 -fy 536 -cx 327 -cy 241 -pose -vis-track ', executable, rootDir, output_dir);      
+     
+if(verbose)
+    command = cat(2, command, [' -tracked ' outputVideo]);
+end  
 
-for i=3:numTogether:numel(dbSeqDir)
-        
-    command = [executable  ' -fx 535 -fy 536 -cx 327 -cy 241 -pose -vis-track '];
+if(any(strcmp('model', varargin)))
+    command = cat(2, command, [' -mloc "', varargin{find(strcmp('model', varargin))+1}, '"']);
+end
 
-    command = cat(2, command, [' -inroot ' '"' rootDir '/"']);
-
-    % deal with edge cases
-    if(numTogether + i > numel(dbSeqDir))
-        numTogether = numel(dbSeqDir) - i + 1;
-    end
-    
-    for n=0:numTogether-1
-        
-        inputFile = [ictDir dbSeqDir(i+n).name '/colour undist.avi'];
-        
-        command = cat(2, command,  [' -f "' inputFile '" -of "' output_dir  '" ']);
-                    
-    end
-    
-    if(verbose)
-        command = cat(2, command, [' -tracked ' outputVideo]);
-    end
-    
-    if(any(strcmp('model', varargin)))
-        command = cat(2, command, [' -mloc "', varargin{find(strcmp('model', varargin))+1}, '"']);
-    end    
-        
-    if(isunix)
-        unix(command, '-echo')
-    else
-        dos(command);
-    end
+for i=1:numel(dbSeqDir)
+    inputFile = [ictDir dbSeqDir(i).name '/colour undist.avi'];
+    command = cat(2, command,  [' -f "' inputFile '" -of "' dbSeqDir(i).name  '" ']);
+end
+         
+if(isunix)
+    unix(command, '-echo')
+else
+    dos(command);
 end
 
 
