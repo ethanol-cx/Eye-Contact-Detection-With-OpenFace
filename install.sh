ea64969e513a6799600ec8ae92a8b64f32e996e8
Tadas Baltrusaitis <tadyla@gmail.com>
2018-02-02 17:31:14 +0000

    Changes to install script

--- install.sh
+++ install.sh
@@ -37,18 +37,18 @@ echo "Essential dependencies installed."
 
 # OpenCV Dependency
 echo "Downloading OpenCV..."
-wget https://github.com/Itseez/opencv/archive/3.1.0.zip
-unzip 3.1.0.zip
-cd opencv-3.1.0
+wget https://github.com/opencv/opencv/archive/3.4.0.zip
+unzip 3.4.0.zip
+cd opencv-3.4.0
 mkdir -p build
 cd build
 echo "Installing OpenCV..."
-cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_SHARED_LIBS=OFF ..
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D WITH_CUDA=OFF -D BUILD_SHARED_LIBS=OFF ..
 make -j4
 sudo make install
 cd ../..
-rm 3.1.0.zip
-sudo rm -r opencv-3.1.0
+rm 3.4.0.zip
+sudo rm -r opencv-3.4.0
 echo "OpenCV installed."
 
 # Boost C++ Dependency
